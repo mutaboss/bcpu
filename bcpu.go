6154902446fdf6fd15d2cb01b03657f9730d6e18
Brian P King <brian@jenashcal.net>
2021-05-12 19:17:08 -0700

    Added OpSetReg

--- bcpu.go
+++ bcpu.go
@@ -1,17 +1,24 @@
 package main
 
+import (
+    "fmt"
+)
+
 const DefaultMemorySize = 4096
 const ProgramStart = 256
 const InstructionWidth = 2
+const RegisterCount = 16
 
 const (
     OpHalt = iota
     OpNoop
+    OpSetReg
 )
 
 type Bcpu struct {
     pc int
-    memory [DefaultMemorySize]int16
+    memory [DefaultMemorySize]uint16
+    register [RegisterCount]uint16
 }
 
 func NewBcpu() *Bcpu {
@@ -28,20 +35,36 @@ func (cpu *Bcpu) ProgramCounter() int {
     return cpu.pc
 }
 
-func (cpu *Bcpu) SetMemory(location int, value int16) {
+func (cpu *Bcpu) SetMemory(location int, value uint16) {
     cpu.memory[location] = value
 }
 
-func (cpu *Bcpu) Run() {
-    cpu.pc = ProgramStart
-    for {
-        inst, _ := cpu.memory[cpu.pc], cpu.memory[cpu.pc+1]
-        cpu.pc = cpu.pc + 2
-        if inst == OpHalt {
-            break
-        } else if inst == OpNoop {
-            /* do nothing */
-        }
+func (cpu *Bcpu) GetRegister(reg int) (uint16, error) {
+    if reg < 0 || reg > RegisterCount - 1 {
+        return 0, fmt.Errorf("Bad register designation: %d.", reg)
     }
+    return cpu.register[reg], nil
 }
 
+func (cpu *Bcpu) Run() error {
+    cpu.pc = ProgramStart
+    for {
+        inst, param := cpu.memory[cpu.pc], cpu.memory[cpu.pc+1]
+        opcode := (inst & 0xFF00) >> 8
+        target := inst&0x00FF
+        cpu.pc = cpu.pc + InstructionWidth
+        if opcode == OpHalt {
+            break
+        } else if opcode == OpNoop {
+            /* do nothing */
+        } else if opcode == OpSetReg {
+            if target < 0 || target > RegisterCount - 1 {
+                return fmt.Errorf("Invalid register %d.", target)
+            }
+            cpu.register[target] = param
+        } else {
+            return fmt.Errorf("Invalid opcode: %d.", opcode)
+        }
+    }
+    return nil
+}
--- bcpu_test.go
+++ bcpu_test.go
@@ -17,7 +17,9 @@ func TestBcpuNew(t *testing.T) {
 
 func TestBcpuRun(t *testing.T) {
     var cpu *Bcpu = NewBcpu()
-    cpu.Run()
+    if err := cpu.Run(); err != nil {
+        t.Error(fmt.Sprintf("Execution error: %s.", err))
+    }
     if cpu.ProgramCounter() != ProgramStart + InstructionWidth {
         t.Error(fmt.Sprintf("Program Counter did not advance (is %d).", cpu.ProgramCounter()))
     }
@@ -25,10 +27,64 @@ func TestBcpuRun(t *testing.T) {
 
 func TestBcpuNoop(t *testing.T) {
     var cpu *Bcpu = NewBcpu()
-    cpu.SetMemory(ProgramStart, OpNoop)
-    cpu.SetMemory(ProgramStart+2, OpNoop)
-    cpu.Run()
-    if cpu.ProgramCounter() != ProgramStart + 3*InstructionWidth {
-        t.Error(fmt.Sprintf("Program Counter should be %d, is %d.", ProgramStart + 2, cpu.ProgramCounter()))
+    cpu.SetMemory(ProgramStart, OpNoop<<8)
+    cpu.SetMemory(ProgramStart+InstructionWidth, OpNoop<<8)
+    if err := cpu.Run(); err != nil {
+        t.Error(fmt.Sprintf("Execution error: %s.", err))
     }
+    exppc := ProgramStart + 3*InstructionWidth
+    if cpu.ProgramCounter() != exppc {
+        t.Error(fmt.Sprintf("Program Counter should be %d, is %d.", exppc, cpu.ProgramCounter()))
+    }
+}
+
+func TestBadOpcode(t *testing.T) {
+    var cpu *Bcpu = NewBcpu()
+    cpu.SetMemory(ProgramStart, 0xffff)
+    if err := cpu.Run(); err == nil {
+        t.Error("Expected 0xFFFF to be a bad opcode.")
+    }
+}
+
+func testRegisterGet(cpu *Bcpu, reg int, expval uint16) bool {
+    val, err := cpu.GetRegister(reg)
+    if err != nil {
+        return false
+    }
+    if val != expval {
+        return false
+    }
+    return true
+}
+
+func testRegisterSet(t *testing.T, cpu *Bcpu, reg uint16, expval uint16) {
+    cpu.SetMemory(ProgramStart, OpSetReg<<8 + reg)
+    cpu.SetMemory(ProgramStart+1, expval)
+    if err := cpu.Run(); err != nil {
+        t.Error(fmt.Sprintf("Execution error: %s", err))
+    }
+    val, err := cpu.GetRegister(int(reg))
+    if err != nil {
+        t.Error(err)
+    }
+    if val != expval {
+        t.Error(fmt.Sprintf("Expected register %d to be %d, was %d.", reg, expval, val))
+    }
+}
+
+func TestBcpuRegisters(t *testing.T) {
+    var cpu *Bcpu = NewBcpu()
+    if ! testRegisterGet(cpu, 0, 0) {
+        t.Error(fmt.Sprintf("Expected register %d to be good, and have a value of %d.", 0, 0))
+    }
+    if ! testRegisterGet(cpu, RegisterCount-1, 0) {
+        t.Error(fmt.Sprintf("Expected register %d to be good, and have a value of %d.", RegisterCount-1, 0))
+    }
+}
+
+func TestBcpuOpSetreg(t *testing.T) {
+    var cpu *Bcpu = NewBcpu()
+    testRegisterSet(t, cpu, 0, 16)
+    testRegisterSet(t, cpu, 1, 256)
+    testRegisterSet(t, cpu, RegisterCount-1, 256)
 }
